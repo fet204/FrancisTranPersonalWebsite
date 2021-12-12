3bd6a6f147c1a3df336eab90497fc29532aff7b4
Frank Tran <306933@gmail.com>
2021-12-12 14:25:59 -0800

    Successfully put the nav bar into skeleton pagegit add .git add .git add .git add .git add .git add .git add .git add .git add . My god. Nice job man!

---
+++ My stuff/js/main.js
@@ -0,0 +1,37 @@
+// Dropdowns.
+$('.nav > ul').dropotron({
+    mode: 'fade',
+    speed: 350,
+    noOpenerFade: true,
+    alignment: 'center'
+});
+/*
+// Nav.
+
+		// Button.
+        $(
+            '<div id="navButton">' +
+                '<a href="#navPanel" class="toggle"></a>' +
+            '</div>'
+        )
+            .appendTo($body);
+
+    // Panel.
+        $(
+            '<div id="navPanel">' +
+                '<nav>' +
+                    $('#nav').navList() +
+                '</nav>' +
+            '</div>'
+        )
+            .appendTo($body)
+            .panel({
+                delay: 500,
+                hideOnClick: true,
+                hideOnSwipe: true,
+                resetScroll: true,
+                resetForms: true,
+                target: $body,
+                visibleClass: 'navPanel-visible'
+            });
+            */
